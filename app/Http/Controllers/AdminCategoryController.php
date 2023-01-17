6b4cce42eca600df53ab98f425d2b1b7894c2f96
Dias Utsman <utsmand91@gmail.com>
2023-01-17 09:28:14 +0700

    Fix bug when edit category with same name as is
    it count as pass when that happen

--- app/Http/Controllers/AdminCategoryController.php
+++ app/Http/Controllers/AdminCategoryController.php
@@ -71,7 +71,7 @@ class AdminCategoryController extends Controller
   public function update(Request $request, Category $category)
   {
     $validatedData = $request->validate([
-      'name' => 'required|max:255|unique:categories',
+      'name' => 'required|max:255|unique:categories,name,' . $category->id . ',id'
     ]);
 
     $validatedData['slug'] = SlugService::createSlug(Category::class, 'slug', $validatedData['name']);
